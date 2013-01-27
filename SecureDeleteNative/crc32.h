2195d250e71705ed5205734ce3dadd72ac138bb4
Gratian Lup <mail@gratianlup.com>
2013-01-27 21:10:03 +0200

    Updated copyright notice.

--- SecureDeleteNative/crc32.h
+++ SecureDeleteNative/crc32.h
@@ -1,7 +1,5 @@
 // Copyright (c) 2005 Gratian Lup. All rights reserved.
 // Redistribution and use in source and binary forms, with or without
-// Copyright (c) Gratian Lup. All rights reserved.
-// Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
 //
